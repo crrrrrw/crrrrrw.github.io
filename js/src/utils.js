9579d703fa4c65133b9340a3d876b4ab2b8d78a6
陈瑞文 <chenruiwen@chenruiwen-mbp.lan>
2018-08-04 23:16:32 +0800

    浪里个浪，博主又有更新啦: 2018-08-04 23:16:31

--- js/src/utils.js
+++ js/src/utils.js
@@ -7,9 +7,10 @@ NexT.utils = NexT.$u = {
   wrapImageWithFancyBox: function () {
     $('.content img')
       .not('[hidden]')
-      .not('.group-picture img, .post-gallery img')
+      // .not('.group-picture img, .post-gallery img')
       .each(function () {
         var $image = $(this);
+        if ($(this).hasClass('nofancybox')) return;
         var imageTitle = $image.attr('title');
         var $imageWrapLink = $image.parent('a');
 
